51af721bbf7d5a478514bc37dcd3a769e5df8f65
shouldhavedone <wt15182161727@163.com>
2019-12-29 21:12:51 +0800

    共享结构的对象提高性能

--- make-redux/src/index.js
+++ make-redux/src/index.js
@@ -4,7 +4,7 @@
  * @Author: WuTao
  * @Date: 2019-12-29 11:10:16
  * @LastEditors  : WuTao
- * @LastEditTime : 2019-12-29 20:41:40
+ * @LastEditTime : 2019-12-29 21:12:27
  */
 // import React from 'react';
 // import ReactDOM from 'react-dom';
@@ -28,37 +28,64 @@ let appState = {
     color: 'red'
   }
 }
-function renderApp (appState) {
-  renderTitle(appState.title)
-  renderContent(appState.content)
+function renderApp (newAppState, oldAppState={}) { // 防止oldAppState没有传
+  if(newAppState === oldAppState) return // 数据没有变化就不渲染
+  console.log('render app ...')
+  renderTitle(newAppState.title, oldAppState.title)
+  renderContent(newAppState.content, oldAppState.content)
 }
 
-function renderTitle (title) {
+function renderTitle (newTitle, oldTitle={}) {
+  if(newTitle === oldTitle) return // 数据没变化就不渲染
+  console.log('render title ...')
   const titleDOM = document.getElementById('title')
-  titleDOM.innerHTML = title.text
-  titleDOM.style.color = title.color
+  titleDOM.innerHTML = newTitle.text
+  titleDOM.style.color = newTitle.color
 }
 
-function renderContent (content) {
+function renderContent (newContent, oldContent={}) {
+  if(newContent === oldContent) return 
+  console.log('render content ...')
   const contentDOM = document.getElementById('content')
-  contentDOM.innerHTML = content.text
-  contentDOM.style.color = content.color
+  contentDOM.innerHTML = newContent.text
+  contentDOM.style.color = newContent.color
 }
 renderApp(appState)
 
-function stateChanger(action){
+// function stateChanger(action){
+//   switch(action.type){
+//     case 'UPDATE_TITLE_TEXT':
+//       appState.title.text = action.text
+//       break
+//     case 'UPDATE_TITLE_COLOR':
+//       appState.title.color = action.color
+//       break
+//     default:
+//       break
+//   }
+// }
+function stateChanger(state, action){
   switch(action.type){
     case 'UPDATE_TITLE_TEXT':
-      appState.title.text = action.text
-      break
+      return { // 构建新的对象并且返回
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
     case 'UPDATE_TITLE_COLOR':
-      appState.title.color = action.color
-      break
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
     default:
-      break
+      return state // 没有修改，返回原来的对象
   }
 }
-
 // dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js》' }) // 修改标题文本
 // dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
 // renderApp(appState) // 把新的数据渲染到页面上.
@@ -74,17 +101,26 @@ function createStore(state, stateChanger){
   const subscribe = (listener) => listeners.push(listener)
   const getState = () => state
   const dispatch = (action) => {
-    stateChanger(state, action)
+    //stateChanger(state, action)
+    state = stateChanger(state, action) // 覆盖原对象
     listeners.forEach((listener) => listener())
   }
   return {getState, dispatch, subscribe}
 }
 
+// const store = createStore(appState, stateChanger)
+// store.subscribe(() => renderApp(store.getState()))// 监听数据变化
+
+// renderApp(store.getState())
+// store.dispatch({type: 'UPDATE_TITLE_TEXT', text: '《React.js》'})
+// store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'blue'})
+
+// renderApp(store.getState())
+
 const store = createStore(appState, stateChanger)
-store.subscribe(() => renderApp(store.getState()))// 监听数据变化
-
-renderApp(store.getState())
-store.dispatch({type: 'UPDATE_TITLE_TEXT', text: '《React.js》'})
-store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'blue'})
-
-renderApp(store.getState())
+let oldState = store.getState() // 缓存旧的state
+store.subscribe(() => {
+  const newState = store.getState() // 数据可能变化，获取新的state
+  renderApp(newState, oldState) // 把新旧的state传进去渲染
+  oldState = newState // 渲染完后，新的state变成旧的state
+})
